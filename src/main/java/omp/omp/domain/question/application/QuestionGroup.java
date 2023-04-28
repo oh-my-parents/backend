3ac312ef67cda842887ec1ca286956615ebd5679
Jinhoon Park <g22203@naver.com>
2023-04-28 19:29:38 +0900

    feat : modify questGroup (#4)

--- src/main/java/omp/omp/domain/question/application/QuestionGroup.java
+++ src/main/java/omp/omp/domain/question/application/QuestionGroup.java
@@ -9,12 +9,12 @@ import omp.omp.domain.userquestion.domain.QuestionParentType;
  */
 @Getter
 public enum QuestionGroup {
-    PARENT_AGE("${parent}의 나이는?", 1),
-    PARENT_FAVORITE_FOOD("${parent}이 가장 좋아하는 음식은?", 2),
-    PARENT_FAVORITE_SONG("${parent}이 가장 좋아하는 노래는?", 3),
-    PARENT_HOBBY("${parent}의 취미는 무엇일까요?", 4),
-    PARENT_DREAM("${parent}의 꿈은 뭘까요?", 5),
-    PARENT_THINK_ME("${parent}이 생각하는 나의 모습은?", 8);
+    PARENT_AGE("%s의 나이는?", 1),
+    PARENT_FAVORITE_FOOD("%s이 가장 좋아하는 음식은?", 2),
+    PARENT_FAVORITE_SONG("%s이 가장 좋아하는 노래는?", 3),
+    PARENT_HOBBY("%s의 취미는 무엇일까요?", 4),
+    PARENT_DREAM("%s의 꿈은 뭘까요?", 5),
+    PARENT_THINK_ME("%s이 생각하는 나의 모습은?", 8);
 
     private final String message;
     private final int order;
@@ -25,6 +25,6 @@ public enum QuestionGroup {
     }
 
     public String getMessagePlusParentType(QuestionParentType questionParentType) {
-        return message.replaceAll("\\$\\{parent}", questionParentType.getParent());
+        return String.format(this.message, questionParentType.getParent());
     }
 }
