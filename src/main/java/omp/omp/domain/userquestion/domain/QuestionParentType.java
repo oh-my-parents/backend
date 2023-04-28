72758c6f9ff4df753f9417e1aca4c6bdf4c6a3dc
Jinhoon Park <g22203@naver.com>
2023-04-28 18:28:19 +0900

    feat : add QuestionParentType (#4)

---
+++ src/main/java/omp/omp/domain/userquestion/domain/QuestionParentType.java
@@ -0,0 +1,21 @@
+package omp.omp.domain.userquestion.domain;
+
+import lombok.Getter;
+
+/**
+ *  질문지에서 구분하기 위함
+ *  FATHER : 아버지용
+ *  MOTHER : 어머니용
+ */
+@Getter
+public enum QuestionParentType {
+
+    FATHER("아버지"),
+    MOTHER("어머니");
+
+    private String parent;
+
+    QuestionParentType(String parent) {
+        this.parent = parent;
+    }
+}
