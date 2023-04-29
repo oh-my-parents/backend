de0f1c8210302fc85defcd3402b77113bf5d1c6a
Jinhoon Park <g22203@naver.com>
2023-04-29 16:10:34 +0900

    feat : add service findQuestionContentsWithParent method (#10)

--- src/main/java/omp/omp/domain/question/application/QuestionService.java
+++ src/main/java/omp/omp/domain/question/application/QuestionService.java
@@ -3,8 +3,13 @@ package omp.omp.domain.question.application;
 import lombok.RequiredArgsConstructor;
 import omp.omp.domain.question.dao.QuestionRepository;
 import omp.omp.domain.question.domain.Question;
+import omp.omp.domain.userquestion.domain.ParentType;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional(readOnly = true)
@@ -24,4 +29,19 @@ public class QuestionService {
 
         return question.getId();
     }
+
+    public List<String> findQuestionContentsWithParent(ParentType parentType) {
+        List<Question> questions = questionRepository.findAll();
+
+        sortQuestionByOrder(questions);
+
+        return questions.stream()
+                .map(m -> m.plusParentType(parentType))
+                .collect(Collectors.toList());
+    }
+
+    private void sortQuestionByOrder(List<Question> questions) {
+        questions.sort(Comparator.comparingInt(Question::getOrderNumber));
+    }
+
 }
