fcff29865ad5aeaf85e7efa9303e5e46ee781130
Jinhoon Park <g22203@naver.com>
2023-05-02 21:24:16 +0900

    feat : add UserException (#15)

--- src/main/java/omp/omp/domain/user/exception/UserExceptionGroup.java
+++ src/main/java/omp/omp/domain/user/exception/UserExceptionGroup.java
@@ -5,9 +5,10 @@ import lombok.Getter;
 @Getter
 public enum UserExceptionGroup {
 
-    USER_NULL("U001", "해당유저가 만든 부모님타입의 설문지가 없습니다."),
-    USER_SCORE_NULL("U002", "아직 채점이 완료되지 않은 상태입니다.");
-
+    USER_NULL("U001", "유저가 없습니다."),
+    USER_SCORE_NULL("U002", "아직 채점이 완료되지 않은 상태입니다."),
+    USER_QUESTION_INVALID_SAVE("U003", "저장된 질문의 순서와 전달받은 질문의 순서가 올바르지 않습니다."),
+    USER_QUESTION_NULL("U004", "저장된 질문에 대한 답변이 없습니다.");
     private final String userErrorCode;
     private final String desc;
 
