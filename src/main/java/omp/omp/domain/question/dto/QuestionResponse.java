62e54dfad7151f9d8847ed4af199fa8daafb716a
Jinhoon Park <g22203@naver.com>
2023-05-04 21:51:11 +0900

    feat : add question dto (#23)

--- src/main/java/omp/omp/domain/question/dto/QuestionResponse.java
+++ src/main/java/omp/omp/domain/question/dto/QuestionResponse.java
@@ -1,15 +1,20 @@
 package omp.omp.domain.question.dto;
 
 import lombok.Data;
+import omp.omp.domain.question.domain.Question;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Data
 public class QuestionResponse {
 
-    List<String> questionContents;
+    List<QuestionDTO> questionDTOs;
 
-    public QuestionResponse(List<String> questionContents) {
-        this.questionContents = questionContents;
+    public QuestionResponse(List<Question> questions) {
+
+        this.questionDTOs = questions.stream()
+                .map(QuestionDTO::new)
+                .collect(Collectors.toList());
     }
 }
